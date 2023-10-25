c77dec119aff3cb2e5c204e663b1f1999c5d7ce0
Song <mcprincesong@gmail.com>
2023-10-24 20:47:27 -0400

    Add files via upload

--- skillscanner/summarize_results.py
+++ skillscanner/summarize_results.py
@@ -5,9 +5,11 @@ x = os.system('mkdir summary')
 
 for skill in skills:
     report = open('results/' + skill + '/report.txt').read().split('\n')[:-1]
-    for line in report:
+    for line in report[6:]:
         if line == ['']:
             continue
-        with open('summary/' + line.split('\t')[0] + '.txt', 'a') as f:
+        if 'issue' in line.lower():
+            continue
+        with open('summary/' + line.split('\t')[0].split(':')[0].replace('/','_') + '.txt', 'a') as f:
             x = f.write(skill + '\n')
     
